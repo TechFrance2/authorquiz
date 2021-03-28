6aa93577348cbe7a2581abbf8e6a3a82e4deba1e
Franci G <franci@handels.com>
2021-03-28 15:30:54 -0700

    added images and book titles

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import AuthorQuiz from "./AuthorQuiz";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const authors = [
+  {
+    name: "Mark Twain",
+    imageUrl: "images/authors/marktwain.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["The Adventures of Huckleberry Finn"],
+  },
+];
+
+const state = {
+  turnData: {
+    author: authors[0],
+    books: authors[0].books,
+  },
+};
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
