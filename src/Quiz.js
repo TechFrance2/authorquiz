57a9ac172daa172f706d5a8eca6c8a7281d79a49
HarrisMBrad <bmichaelh13@gmail.com>
2021-11-12 15:18:45 -0500

    added files for qquestions, quiz, mock data w/json file and more

---
+++ src/Quiz.js
@@ -0,0 +1,27 @@
+//import react from 'react';
+import { Component } from 'react';
+class Quiz extends Component {
+
+  render() {
+    return (
+      <main>
+        <section>
+          <p>This main section may have buttons to select from in the future... </p>
+        </section>
+        <section className="buttons">
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+          </ul>
+        </section>
+      </main>
+    )
+  }
+}
+
+export default Quiz;
